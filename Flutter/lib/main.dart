9a00e311bedb0efa92228689ed7cc91ba2cfa657
renaldiB <88638213+renaldiB@users.noreply.github.com>
2022-04-20 19:16:23 +0700

    Add files via upload

---
+++ Flutter/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+          backgroundColor: Colors.teal,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Image(
+                image: AssetImage('images/untar.png'),
+                //NetworkImage(
+                //'https://images.unsplash.com/photo-1649859398731-d3c4ebca53fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'),
+              ),
+              Text('Hello World'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
